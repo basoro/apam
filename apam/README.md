25647e2f35334c49e224bdfbedc56d0af85ca410
basoro <dentix.id@gmail.com>
2021-02-25 09:27:35 +0800

    Update

---
+++ apam/README.md
@@ -0,0 +1,9 @@
+# APAM Barabai
+Aplikasi Pasien dan Antrian Mandiri
+
+Versi 3.0.0
+
+# APAM Barabai Rest API
+Untuk versi 3.0.0, APAM Rest API ada didalam modul KhanzaLITE
+
+Untuk pemasangan, silahkan merujuk ke https://basoro.org
---
+++ apam/www/js/routes.js
@@ -0,0 +1,103 @@
+routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+  {
+    path: '/signin/',
+    url: './pages/signin.html',
+  },
+  {
+    path: '/panduan/',
+    url: './pages/panduan.html',
+  },
+  {
+    path: '/home/',
+    url: './pages/home.html',
+  },
+  {
+    path: '/blog/',
+    url: './pages/blog.html',
+  },
+  {
+    path: '/blog/:id/',
+    url: './pages/blog-detail.html',
+  },
+  {
+    path: '/daftar/',
+    url: './pages/daftar.html',
+  },
+  {
+    path: '/kamar/',
+    url: './pages/kamar.html',
+  },
+  {
+    path: '/dokter/',
+    url: './pages/dokter.html',
+  },
+  {
+    path: '/rawatjalan/',
+    url: './pages/rawatjalan.html',
+  },
+  {
+    path: '/rawatinap/',
+    url: './pages/rawatinap.html',
+  },
+  {
+    path: '/laboratorium/',
+    url: './pages/laboratorium.html',
+  },
+  {
+    path: '/radiologi/',
+    url: './pages/radiologi.html',
+  },
+  {
+    path: '/pengaduan/',
+    url: './pages/pengaduan.html',
+  },
+  {
+    path: '/pengaduan/:no_rkm_medis/:id/',
+    url: './pages/pengaduan-detail.html',
+  },
+  {
+    path: '/booking/:no_rkm_medis/:tanggal_periksa/:no_reg/',
+    url: './pages/booking-detail.html',
+  },
+  {
+    path: '/riwayat/:no_rkm_medis/:tgl_registrasi/:no_reg/',
+    url: './pages/riwayat-detail.html',
+  },
+  {
+    path: '/riwayatranap/:no_rkm_medis/:tgl_registrasi/:no_reg/',
+    url: './pages/riwayatranap-detail.html',
+  },  {
+    path: '/profil/',
+    url: './pages/profil.html',
+  },
+  {
+    path: '/sukses/',
+    url: './pages/sukses.html',
+  },
+  {
+    path: '/profile/',
+    url: './pages/profile.html',
+  },
+  {
+    path: '/notifikasi/',
+    url: './pages/notifikasi.html',
+  },
+  // Color Themes
+  {
+    path: '/pengaturan/',
+    componentUrl: './pages/pengaturan.html',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
